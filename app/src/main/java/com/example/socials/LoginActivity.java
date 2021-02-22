54365af76b2fe14e9a726ee8e562b05fb900e15e
Danny393 <danielacosta393@gmail.com>
2021-02-21 19:45:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/socials/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.socials;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnLogin;
+    String TAG = "LoginActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Pressed");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG,"Logging user in: " + username + " with password " + password);
+    }
+}
